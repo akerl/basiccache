ef959626391c31cd0d17f04da96f1b754925d905
Les Aker <me@lesaker.org>
2013-09-30 11:10:22 -0400

    more docs

--- lib/basic_cache.rb
+++ lib/basic_cache.rb
@@ -1,11 +1,23 @@
+##
+# This module provides a simple key/value cache for storing computation results
+
 module Basic_Cache
     Version = '0.0.5'
+
+    ##
+    # Insert a helper .new() method for creating a new Cache object
 
     class << self
         def new(*args)
             self::Cache.new(*args)
         end
     end
+
+    ##
+    # If we're using 2.0.0+, caller_locations is available and is much faster.
+    # If not, fall back to caller
+    # These methods return the name of the calling function 2 levels up
+    # This allows them to return the name of whatever called Cache.cache()
 
     if Kernel.respond_to? 'caller_locations'
         def self.get_caller
@@ -17,16 +29,29 @@ module Basic_Cache
         end
     end
 
+    ##
+    # Cache object, maintains a key/value store
+
     class Cache
         attr_reader :store
+
+        ##
+        # Generate an empty store
 
         def initialize
             @store = Hash.new
         end
 
+        ##
+        # Empty out the store
+
         def clear
             @store.clear
         end
+
+        ##
+        # If the key is cached, return it.
+        # If not, run the code, cache the result, and return it
 
         def cache(key = nil, &code)
             @store[(key || Basic_Cache::get_caller()).to_sym] ||= code.call
