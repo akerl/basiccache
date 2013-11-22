f64cfe98aed55f07084685acbe39633727045a43
Les Aker <me@lesaker.org>
2013-11-22 00:29:11 -0500

    first try

---
+++ lib/caches/timecache.rb
@@ -0,0 +1,56 @@
+##
+# Time-based cache object
+
+module BasicCache::TimeCache
+  class TimeCache < Cache
+    attr_reader :lifetime
+
+    ##
+    # Generate an empty store, with a default lifetime of 60 seconds
+
+    def initialize(lifetime = 30)
+      @lifetime = lifetime
+      @cache_item = Struct.new(:stamp, :value)
+      super()
+    end
+
+    ##
+    # If the key is cached but expired, clear it
+
+    def cache(key = nil, &code)
+      key ||= BasicCache.get_caller
+      key = key.to_sym
+      # rubocop:disable AndOr
+      unless @store.include? key and Time.now - @store[key].stamp < @lifetime
+        @store[key] = @cache_item.new(Time.now, code.call)
+      end
+      # rubocop:enable AndOr
+      @store[key].value
+    end
+
+    ##
+    # Check if a value is cached and not expired
+
+    def include?(key = nil)
+      key ||= BasicCache.get_caller
+      key = key.to_sym
+      # rubocop:disable AndOr
+      @store.include? key and Time.now - @store[key].stamp < @lifetime
+      # rubocop:enable AndOr
+    end
+
+    def [](key = nil)
+      super.value
+    end
+
+    ##
+    # Return the size of the cache (don't include expired entries)
+    # By default, purges expired entries while iterating
+
+    def size(purge = true)
+      valid = @store.select { |k, v| Time.now - v.stamp < @lifetime }
+      @store = valid if purge
+      valid.size
+    end
+  end
+end
